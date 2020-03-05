7d781be033b8c763b58796a2c45710c7cb814b34
Firoozeh Saiepour <firoozeh.saiepour@simplybusiness.co.uk>
2020-03-05 13:35:47 +0000

    Initial test

--- ruby/my_service.rb
+++ ruby/my_service.rb
@@ -1,14 +1,13 @@
-require 'rubygems'
 require 'mqtt'
 
-SERVICE_NAME = 'kittens'
-MQTT_HOST = ENV['MQTT_HOST'] || 'test.mosquitto.org'
+SERVICE_NAME = 'mutation'
+MQTT_HOST = ENV['MQTT_HOST'] || 'mqtt.fluux.io'
 
 # Subscribe example
 MQTT::Client.connect(MQTT_HOST) do |c|
   # If you pass a block to the get method, then it will loop
   # The '#' wildcard subscribes to all topics below this one
-  c.get('uservicehack/#') do |topic,message|
+  c.get("uservicehack/#{SERVICE_NAME}") do |topic,message|
     puts "On topic #{topic} got: #{message}"
   end
 end
--- ruby/publish.rb
+++ ruby/publish.rb
@@ -1,9 +1,9 @@
-require 'rubygems'
 require 'mqtt'
+require 'json'
 
-MQTT_HOST = ENV['MQTT_HOST'] || 'test.mosquitto.org'
+MQTT_HOST = ENV['MQTT_HOST'] || 'mqtt.fluux.io'
 
 # Publish example
 MQTT::Client.connect(MQTT_HOST) do |c|
-  c.publish('uservicehack/kittens', 'I love kittenz!')
+  c.publish('uservicehack/mutation', {infection_rate: 1}.to_json)
 end
