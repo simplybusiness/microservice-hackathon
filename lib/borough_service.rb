1bbd6ed827adf9f55c788a22927aadf44c189b3a
Jayda Kadir <jayda.kadir@simplybusiness.co.uk>
2020-03-05 15:27:08 +0000

    Add ability to find borough by name

--- lib/borough_service.rb
+++ lib/borough_service.rb
@@ -1,3 +1,9 @@
 class BoroughService
+  def initialize(boroughs)
+    @boroughs = boroughs
+  end
 
+  def find(borough_name)
+    @boroughs.detect { |borough| borough.name == borough_name }
+  end
 end
--- spec/borough_service_spec.rb
+++ spec/borough_service_spec.rb
@@ -1,4 +1,11 @@
 require_relative "../lib/borough_service.rb"
 
 RSpec.describe BoroughService do
+
+
+  it 'retrieves the borough given the name' do
+    boroughs = [double(name: "Hackney"), double(name: "Islington")]
+    borough_service = BoroughService.new(boroughs)
+    expect(borough_service.find("Islington")).to eq(boroughs.last)
+  end
 end
