0cba821f4fa8cdf7219ab462312c5821af38454a
Firoozeh Saiepour <firoozeh.saiepour@simplybusiness.co.uk>
2020-03-05 14:10:36 +0000

    Add the time event consumer

--- ruby/my_service.rb
+++ ruby/my_service.rb
@@ -1,13 +1,16 @@
 require 'mqtt'
+require 'json'
 
-SERVICE_NAME = 'mutation'
 MQTT_HOST = ENV['MQTT_HOST'] || 'mqtt.fluux.io'
+
+
 
 # Subscribe example
 MQTT::Client.connect(MQTT_HOST) do |c|
   # If you pass a block to the get method, then it will loop
   # The '#' wildcard subscribes to all topics below this one
-  c.get("uservicehack/#{SERVICE_NAME}") do |topic,message|
+  c.get("uservicehack/time") do |topic,message|
     puts "On topic #{topic} got: #{message}"
+    c.publish('uservicehack/mutation', {infection_rate: 22}.to_json)
   end
 end
--- ruby/publish.rb
+++ ruby/publish.rb
@@ -5,5 +5,7 @@ MQTT_HOST = ENV['MQTT_HOST'] || 'mqtt.fluux.io'
 
 # Publish example
 MQTT::Client.connect(MQTT_HOST) do |c|
-  c.publish('uservicehack/mutation', {infection_rate: 1}.to_json)
+  c.publish('uservicehack/mutation', {infection_rate: 22}.to_json)
 end
+
+
