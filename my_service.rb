6e1be8c1b95518460dd03d80a913597637cd8266
Adam Leach <adam.leach@simplybusiness.co.uk>
2020-03-05 14:09:28 +0000

    Dummy publishing

--- my_service.rb
+++ my_service.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'mqtt'
+require_relative 'lib/borough'
+require 'json'
 
 SERVICE_NAME = 'borough'
 MQTT_HOST = 'mqtt.fluux.io'
@@ -8,7 +10,9 @@ MQTT_HOST = 'mqtt.fluux.io'
 MQTT::Client.connect(MQTT_HOST) do |c|
   # If you pass a block to the get method, then it will loop
   # The '#' wildcard subscribes to all topics below this one
-  c.get('uservicehack/borough') do |topic,message|
-    puts "On topic #{topic} got: #{message}"
+  c.get('uservicehack/time') do |topic,message|
+    boroughs.each do |borough|
+      c.publish('uservicehack/borough', borough.to_json)
+    end
   end
 end
