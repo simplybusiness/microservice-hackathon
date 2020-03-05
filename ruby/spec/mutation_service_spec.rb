9a244d449f3448b3fd89a0959c979bfcfb4d3735
Firoozeh Saiepour <firoozeh.saiepour@simplybusiness.co.uk>
2020-03-05 14:28:17 +0000

    Another Friday

--- ruby/spec/mutation_service_spec.rb
+++ ruby/spec/mutation_service_spec.rb
@@ -1,13 +1,18 @@
 require 'date'
 
 def infection_rate_algorithm(current_rate, date)
-  2
+  current_rate + 1
 end
 
 describe "Mutation Algorithm" do
   it "increments the infection rate every Friday" do
-    #expect(infection_rate_algorithm(current_rate, date).to eq(new_infection_rate))
     expect(infection_rate_algorithm(1, Date.new(2020, 3, 6))).to eq(2)
+  end
+
+  context "Given the current infection rate is 2 and the date is a Friday" do
+    it "increments the infection rate by 1" do
+      expect(infection_rate_algorithm(2, Date.new(2020, 3, 6))).to eq(3)
+    end
   end
 end
 
