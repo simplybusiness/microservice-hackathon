2758d459c333390b87fc4de60415cd5f2717336a
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2020-04-21 12:36:08 +0100

    first test setup

--- ruby/test_my_service.rb
+++ ruby/test_my_service.rb
@@ -1,11 +1,37 @@
 require 'minitest/autorun'
+require 'aws_iot_device'
 
-def meaning_of_life
-  6 * 9
+def create_event(topic:, payload:)
+  @client.publish(topic, payload)
 end
 
-describe "When asked the meaning of life, the universe and everything" do
-  it "must be 42" do
-    _(meaning_of_life).must_equal 42
+def event_published(topic:)
+  @events.any? { |event| event.topic == topic }
+end
+
+describe "the world" do
+  def setup
+    super
+    host = "a267zn9knxsui0-ats.iot.eu-west-1.amazonaws.com"
+    port = 8883
+    certificate_path = "../certs/certificate.pem.crt"
+    private_key_path = "../certs/private.pem.key"
+    root_ca_path = "../certs/root-CA.pem"
+    @events = []
+    @client = AwsIotDevice::MqttShadowClient::MqttManager.new(host: host, port: port, ssl: true)
+    @client.config_ssl_context(root_ca_path, private_key_path, certificate_path)
+    @client.connect
+    callback = Proc.new do |message|
+      @events.append(message)
+    end
+    @client.subscribe("workshop/+", 0, callback)
+  end
+
+  describe "when time starts" do
+    it "starts the world" do
+      create_event(topic: "workshop/time/tick", payload: {"hours_elapsed" => 0})
+
+      assert event_published(topic: "workshop/environment/big_bang"), "Cannot find big bang event"
+    end
   end
 end
